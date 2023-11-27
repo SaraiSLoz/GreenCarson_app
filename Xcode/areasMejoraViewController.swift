f8de8be75210aaf9cc0f714b5cde33bc765d0f79
LACG24 <LACG24@outlook.com>
2023-11-27 00:18:18 -0600

    Implementada descarga

--- Xcode/areasMejoraViewController.swift
+++ Xcode/areasMejoraViewController.swift
@@ -6,6 +6,10 @@
 //
 
 import UIKit
+import SwiftUI
+import Firebase
+import Charts
+import SnapKit
 
 class areasMejoraViewController: UIViewController {
 
@@ -16,8 +20,28 @@ class areasMejoraViewController: UIViewController {
         let backButton = UIBarButtonItem()
         backButton.title = ""
         navigationItem.backBarButtonItem = backButton
-        
-        // Do any additional setup after loading the view.
+        setupView()
     }
 
+    func setupView() {
+        // Obtén la referencia a ageHistoryView desde el Storyboard usando el tag
+        guard let ageHistoryView = self.view.viewWithTag(1) else {
+            return
+        }
+
+        let controller = UIHostingController(rootView: AgeHistory())
+        guard let timeView = controller.view else {
+            return
+        }
+
+        ageHistoryView.addSubview(timeView)
+
+        timeView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview().offset(-40)
+            make.leading.equalToSuperview().offset(25)
+            make.trailing.equalToSuperview().inset(25)
+            make.height.equalTo(250)
+        }
+    }
+    
 }
